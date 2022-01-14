428a94e90886c6d06cb1d3ea3e1beb862d97454a
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-15 00:56:20 +0200

    add task2

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+
+{
+const numberOfItem = document.querySelectorAll("li.item");
+
+console.log(`Number of categories: `, numberOfItem.length);
+}
+
+{
+    const numberOfItem = document.querySelectorAll("li.item");
+    titleOfItem.forEach(("li.item") => {
+        h2.TextContent;
+        
+    });
+    // console.log(titleOfItem);
+}
+console.log(`Category: `, li.h2.TextContent);
+        console.log(`Elements: `, li.ul.length);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,29 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients =
+  [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
+  
+const ingredientsEl = document.querySelector(`#ingredients`);
+
+const ingredientsList = ingredients.forEach((ingredient) => {
+  const ingredientsItem = document.createElement("li");
+  ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList = "item";
+  ingredientsEl.append(ingredientsItem);
+  console.log(ingredientsItem);
+ 
+});
+
+
+  
+  
+
+
+
+
+
