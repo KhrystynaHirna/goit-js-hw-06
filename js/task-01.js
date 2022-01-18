6c721b63f18d94f06bfb4022cdd0f96c1fdbbbec
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-18 17:32:53 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,24 @@
 
-{
-const numberOfItem = document.querySelectorAll("li.item");
+
+const numberOfItem = document.querySelectorAll("ul#categories li.item");
+// console.log(numberOfItem);
 
 console.log(`Number of categories: `, numberOfItem.length);
+
+
+for (let i = 0; i < numberOfItem; i += 1) {
+    const headerText = numberOfItem[i].firstElementChild.textContent;
+    console.log(`Category: ${headerText}`);
+
 }
 
-{
-    const numberOfItem = document.querySelectorAll("li.item");
-    titleOfItem.forEach(("li.item") => {
-        h2.TextContent;
+// const ElementsEl = numberOfItem[i].lastElementChild;
+// const numberOfElementsEl = ElementsEl.querySelectorAll("li");
+
+// numberOfItem.forEach(function (i) {
+   
+//     // if (Elements < numberOfItem.length) {
+       
         
-    });
-    // console.log(titleOfItem);
-}
-console.log(`Category: `, li.h2.TextContent);
-        console.log(`Elements: `, li.ul.length);
+//         console.log(`Elements: `, numberOfElementsEl[i].lastElementChild.length);
+    // });
