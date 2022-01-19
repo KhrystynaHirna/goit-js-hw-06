a0fde741e11f987c744ec24be56bb57cadc4e513
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-19 13:20:00 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,24 +1,20 @@
 
 
-const numberOfItem = document.querySelectorAll("ul#categories li.item");
-// console.log(numberOfItem);
+    const numberOfItem = document.querySelectorAll("ul#categories li.item");
+    // console.log(numberOfItem);
 
-console.log(`Number of categories: `, numberOfItem.length);
+    console.log(`Number of categories: `, numberOfItem.length);
 
-
-for (let i = 0; i < numberOfItem; i += 1) {
-    const headerText = numberOfItem[i].firstElementChild.textContent;
-    console.log(`Category: ${headerText}`);
-
-}
-
-// const ElementsEl = numberOfItem[i].lastElementChild;
-// const numberOfElementsEl = ElementsEl.querySelectorAll("li");
-
-// numberOfItem.forEach(function (i) {
-   
-//     // if (Elements < numberOfItem.length) {
-       
+    numberOfItem.forEach(function (i) {
         
-//         console.log(`Elements: `, numberOfElementsEl[i].lastElementChild.length);
-    // });
+    if (i < numberOfItem.length) {
+       
+        const headerText = numberOfItem[i].firstElementChild.textContent;
+        console.log(`Category: ${headerText}`);
+
+        const ElementsEl = numberOfItem[i].lastElementChild;
+        const numberOfElementsEl = ElementsEl.querySelectorAll("li");
+
+        console.log(`Elements: `, numberOfElementsEl[i].lastElementChild.length);
+     }
+    });
