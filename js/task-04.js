95005d3e46716ff81c216c4839adbebd21c17e7a
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-20 14:22:03 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,28 +1,30 @@
 
-let counterValue = 0; 
+let counterValue = 0;
+const step = 1;
+
 const buttonDecrement = document.querySelector('[data-action="decrement"]');
 const buttonIncrement = document.querySelector('[data-action="increment"]');
 const counterValues = document.querySelector("#value");
 
-buttonDecrement.addEventListener('click', onButtonDecrement => {
-    for (let i = 0; i = 'click'; i += 1) {
 
-        counterValue += i;
-    }
+buttonDecrement.addEventListener('click', () => {
+    counterValue -= step;
+    console.log(counterValue);
+});
+ 
+buttonIncrement.addEventListener('click', () => {
+     counterValue += step;
+     console.log(counterValue);
 });
 
-function onButtonDecrement(event) {
-console.log(event);
-counterValue -= 1;  
-}
-console.log(counterValue);
 
 
-// buttonIncrement.addEventListener('click', event => {
+// const updateCounterValues = function () {
+//     counterValues.textContent = this.counterValue;
+// }
 
-// console.log(event);
-// counterValue += 1;
+counterValues.addEventListener( () => {
+    buttonDecrement.bind(counterValue);
 
-// });
+});
 
-// console.log(counterValue);
