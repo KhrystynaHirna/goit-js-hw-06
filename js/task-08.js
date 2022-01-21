d0a56e2bbf1f44421826511c27813b26e355cf73
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-21 22:52:04 +0100

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+     return alert('please fill the form');
+    }
+    
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+});
 
