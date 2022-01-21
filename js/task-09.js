06ab4d7696abe9f0e37a86842ec68006f06bfe13
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-22 00:30:10 +0100

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonChangeColor = document.querySelector(".change-color");
+const bodyChangeColor = document.querySelector('body');
+const titleChangeColor = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', (event) => {
+bodyChangeColor.style.backgroundColor = getRandomHexColor(event);
+ 
+titleChangeColor.textContent = getRandomHexColor(event);
+  
+console.log(bodyChangeColor);
+});
