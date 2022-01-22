380ecad746babcca8d5aaf00f4cf58e92d29b802
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-22 02:16:10 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,50 +1,31 @@
 
-let counterValue = 0;
-const step = 1;
+let initialCounterValue = 0;
+let step = 1;
 
 const buttonDecrement = document.querySelector('[data-action="decrement"]');
 const buttonIncrement = document.querySelector('[data-action="increment"]');
-const counterValues = document.querySelector("#value");
+const counterValueUpdate = document.querySelector("#value");
 
 
 buttonDecrement.addEventListener('click', (event) => {
-    counterValue -= step;
   
-    counterValues.textContent = event.currentTarget.$`{counterValue }`;
-    console.log(counterValues);
+    initialCounterValue -= step;
+    event.currentTarget.value = initialCounterValue;
+ 
+    counterValueUpdate.textContent = event.currentTarget.value;
 
+    console.log(counterValueUpdate);
 });
  
+
 buttonIncrement.addEventListener('click', (event) => {
-    initialValue += step;
-    counterValue.textContent = event.target.value;
-     console.log(counterValue);
-    
+  
+    initialCounterValue += step;
+    event.currentTarget.value = initialCounterValue;
+ 
+    counterValueUpdate.textContent = event.currentTarget.value;
+
+    console.log(counterValueUpdate);
 });
 
-
-// CounterPlugin.prototipe.updateCounterValue = function () {
-//      counterValues.textContent = counterValue;
-// }
-
-// CounterPlugin.prototipe.buttonDecrement = function () {
-//      counterValue -= step;
-// }
-
-// CounterPlugin.prototipe.buttonIncrement = function () {
-//     counterValue += step;
-// }
-
-// const updateCounterValues = function () {
-//     counterValues.textContent = buttonDecrement.bind(counterValue);
-// }
-
-// function showTargets(counterValues) {
-//     console.log("div target", counterValues.target);
-
-// }
-// counterValues.addEventListener( () => {
-//     buttonDecrement.bind(counterValue);
-
-// });
 
