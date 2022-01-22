eaf51a4af8b10dec6e21cb6837ace38e370bb508
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-01-22 14:36:24 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumber = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const divBoxes = document.querySelector('#boxes');
+
+let initialBoxWidth = 30;
+let initialBoxHeight = 30;
+
+const createBoxes = (amount) => {
+  for (let i = 0; i < inputNumber.value; i += 1) {
+    const divBoxesNew = document.createElement('div');
+    divBoxesNew.style.width = `${ initialBoxWidth += 10 }px`;
+    divBoxesNew.style.height = `${initialBoxHeight += 10}px`;
+    divBoxesNew.style.backgroundColor = getRandomHexColor();
+
+
+    console.log(divBoxesNew);
+    divBoxes.append(divBoxesNew);
+  }
+}
+
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", (createBoxes) => {
+  divBoxes.innerHTML = "";
+  inputNumber.value = "";
+});
